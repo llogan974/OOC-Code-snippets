3dc2dc73ac5bb45739f815b9ab679300287569a3
Luke Logan <llogan382@gmail.com>
2019-03-19 05:33:14 -0400

    add text or html to an element using jquery

---
+++ Jquery/addHTMLtoId.js
@@ -0,0 +1,9 @@
+
+// Content to add
+const title = "my first blog post";
+// HTML to add
+const content = "my <strong>first</strong> post";
+
+// Select the ID then add content as either text or HTML
+$('#blogTitlePreview').text(title);
+$('#blogContentPreview').html(content);
