780d7496d2675d02b7c1d662665ea00730cc5ac2
Luke Logan <llogan382@gmail.com>
2019-03-19 05:16:20 -0400

    jquery for adding a temporary message at top of screen

---
+++ Jquery/slideInMessage.js
@@ -0,0 +1,10 @@
+$('#flashMessage')
+    .hide();
+// Number of Milliseconds used for message to fade in
+    .fadeIn(1000);
+
+// Number of milliseconds for user to read message
+    .delay(3000);
+
+
+    .slideUp();
